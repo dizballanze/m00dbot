ca84a0bcc92fc8ebd9c3ad337eccc4cd12fb86a7
Elayne Lemos <elayne.l.lemos@gmail.com>
2020-02-03 09:12:50 -0300

    Fix name of start button at pt option

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def start(bot, update):
     langs_markup = InlineKeyboardMarkup([[
         InlineKeyboardButton('English {}'.format(b'\xF0\x9F\x87\xAC\xF0\x9F\x87\xA7'.decode()), callback_data='en'),
         InlineKeyboardButton('Русский {}'.format(b'\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA'.decode()), callback_data='ru'),
-        InlineKeyboardButton('Русский {}'.format(b'\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA'.decode()), callback_data='pt')]])
+        InlineKeyboardButton('Português {}'.format(b'\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA'.decode()), callback_data='pt')]])
     bot.send_message(
         text='Choose your language / Выберите язык / Escolha seu idioma', reply_markup=langs_markup, chat_id=update.message.chat_id)
 
