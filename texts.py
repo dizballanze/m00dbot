4a83b6b65f05374616149382526d499421edb145
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-03-30 21:01:10 +0200

    some corrections
    Corrections related to the traduction to Italian + inserted some missing commas

--- texts.py
+++ texts.py
@@ -51,8 +51,8 @@ INTRO = {
           '/madrs - teste de depressão (Escada de Classificação de Depressão de Montgomery–Åsberg)\n'
           '/start - configurações\n'
           '/export - exportar resultados',
-    'it': '🚀 Ciao, amico! Here is the simple and useful chat bot that helps '
-          'per tenere traccia dei problemi dell\'umore (ansia e depressione). Puoi ricevere '
+    'it': '🚀 Ciao, amico! Questo è un semplice e utile charbot che ti aiuta '
+          'a tenere traccia dei problemi dell\'umore (ansia e depressione). Puoi ricevere '
           'comodi promemoria di volta in volta oppure effettuare i test manualmente. '
           'Mantieni la calma 🌼\n\n'
           '/hars - test dell\'ansia (Scala di valutazione dell\'ansia di Hamilton)\n'
@@ -83,8 +83,8 @@ HARS_INTRO = {
     'pt': 'Abaixo está uma lista de frases que descrevem alguns sentimentos que as pessoas têm. '
           'Classifique os pacientes indicando '
           'a resposta que melhor descreve a extensão em que ele/ela tem essas condições.',
-    'it': 'Di seguito è riportato un elenco di frasi che descrivono una certa sensazione che le persone hanno. '
-          'Vota i pazienti trovando la risposta che meglio descrive la misura per cui lui/lei presenta queste '
+    'it': 'Di seguito è riportato un elenco di frasi che descrivono una certa sensazione che le persone provano. '
+          'Vota i pazienti trovando la risposta che meglio descrive la misura in cui lui/lei presenta queste '
           'condizioni.',
 }
 
@@ -112,9 +112,10 @@ MADRS_INTRO = {
           'de 0 a 6, sendo 0 "normal/não apresenta" e 6 sendo "extrema".\n'
           'Os números 1, 3 e 5 não possuem classificação própria na gravidade, na verdade, esses números são '
           'representativos nos níveis entre as duas escolhas.',
-    'it': 'Nota che: Questo test è in genere somministrato da un clinico di terze parti, ma viene fornito qui per '
-          'scopi informativi. Questo test potrebbe non essere accurato quando viene auto-somministrato, dato che si basa su una valutazione obiettiva.\n'
-          'Queste domande riguardano come il paziente si è sentito nella scorsa settimana. Valuta ogni elemento su una scala a partire da 0 fino a 6, con 0 che significa “normale/non presente" e 6 “estremo.”\n'
+    'it': 'Per favore, nota che: Questo test è in genere somministrato da un clinico di terze parti, ma viene fornito qui per '
+          'scopi informativi. Questo test potrebbe non essere accurato quando viene auto-somministrato, dato che si basa su una valutazione alquanto soggettiva.\n'
+          'Queste domande riguardano come il paziente si sia sentito nella scorsa settimana. Valuta ogni elemento su una scala '
+          ' da 0 (“normale/non presente") a 6 (“estremo.”) \n'
           'I numeri 1, 3, e 5 non hanno la propria valutazione testuale di gravità, piuttosto, questi numeri sono '
           'rappresentativi dei livelli tra le due scelte.'
 }
@@ -123,5 +124,5 @@ EXPORT = {
     'en': 'Choose export format:',
     'ru': 'Выберите формат экспорта:',
     'pt': 'Escolha o formato de exportação:',
-    'it': 'Scegli il formato di esportazione:'
+    'it': 'Scegli il formato in cui esportare i tuoi dati:'
 }
